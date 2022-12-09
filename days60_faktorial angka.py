b6065d4610247aacb1b83a80581e666815cee612
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-09 20:38:06 +0800

    Add files via upload

---
+++ days60_faktorial angka.py
@@ -0,0 +1,11 @@
+#program menghitung  menggunakan faktorial
+
+hasil =  1
+
+angka = int (input ("masukkan angka : "))
+
+for i in range (1, angka +1):
+	hasil = hasil * i
+print( "faktorial", angka, "=" , hasil)
+	
+	
